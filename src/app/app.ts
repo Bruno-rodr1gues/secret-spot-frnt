e44fa8de6785ded79bad9c0ae865545656ac6db1
Bruno-rodr1gues <brunno.rodrigues0706@gmail.com>
2026-06-28 17:53:33 -0300

    [front] - instalação do primeng, prime icons e criação do componente header.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,20 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Sidebar } from '../layout/sidebar/sidebar';
+import { Component, OnInit, signal } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { Reader } from '../layout/sidebar/reader';
+import { PrimeNG } from 'primeng/config';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Sidebar],
+  imports: [RouterOutlet, Reader],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('secret-spot-app');
+
+  constructor(private primeng: PrimeNG) {}
+
+  ngOnInit(): void {
+    this.primeng.ripple.set(true);
+  }
 }
